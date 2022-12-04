86cd888f96f32d6830f6a00e1c8ddc7a0c9a7a4f
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2022-12-04 16:00:35 +0700

    benerin summernote

--- app/Views/page/global/dashboard.php
+++ app/Views/page/global/dashboard.php
@@ -2,43 +2,20 @@
 
 <?= $this->section('content') ?>
 
-    <div class="row">
-        <div class="col-12 col-md-6 col-lg-6">
-            <div class="card">
+<div class="row">
+    <div class="col-12 col-md-6 col-lg-6">
+        <div class="card">
 
-                <div class="card-body">
-                    <p>
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                        This is some text within a card body.
-                    </p>
-                </div>
-
+            <div class="card-body">
+                <h2>judul</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dolor quasi mollitia ratione minus blanditiis labore, sed id impedit corporis eum repudiandae inventore officia nam quas ipsum amet commodi ipsam.
+                </p>
             </div>
-        </div>
 
+        </div>
     </div>
+
+</div>
 </div>
 <?= $this->endSection() ?>
